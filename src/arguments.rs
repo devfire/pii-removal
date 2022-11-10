718ac13572edcf6a7eebd4d242b076e79ce2dc01
Igor <igor@devfire.io>
2022-11-10 16:40:53 -0500

    added value_delimiter to handle multipe values

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,10 +1,12 @@
 use clap::Parser;
 
+// NOTE: we need multiple values since shell glob expansion will create N number of files.
+// https://docs.rs/clap/latest/clap/builder/struct.Arg.html#method.get_value_delimiter
 #[derive(Parser,Default,Debug)]
 pub struct Args {
     /// Files glob pattern to process
-    #[clap(short = 'f', long)]
-    pub files: String,
+    #[arg(short = 'f', use_value_delimiter = true, value_delimiter = ' ', long)] //
+    pub files: Option<Vec<String>>,
 
     // Pipe separated regex PII pattern to eliminate, i.e. "CC|SSN"
     //#[clap(short = 'p', long)]
