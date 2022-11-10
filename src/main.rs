c88a853a176318c8f2cd896cf184ae5c280f4bfc
Igor <igor@devfire.io>
2022-11-10 12:59:36 -0500

    pipe as the regex separator

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ There's a way to write a regex! macro which compiles regular expressions when th
 In other words, using regex! means our program cannot compile with an invalid regular expression. 
 Moreover, the regex! macro compiles the given expression to native Rust code, which makes it much faster for searching text.
 
-To use the regex! macro, we must enable the plugin feature and import the regex_macros crate as a syntax extension: 
+To use the regex! macro, we must enable the plugin feature.
 
 Reference: https://docs.rs/regex/0.1.26/regex/
 
-*/
+
 #![feature(plugin)]
 #![plugin(regex_macros)]
 extern crate regex;
+*/
 
 use clap::Parser;
 use crate::arguments::Args;
@@ -38,20 +39,14 @@ fn main() -> io::Result<()> {
    let mut lines_processed = 0;
    let mut lines_redacted = 0;
 
-
-
-
+   let re = Regex::new(r"SSN|CC");
    for line in reader.lines() {
        println!("{}", line?);
        lines_processed = lines_processed + 1;
+
+       
        
    }
-
-   reader
-   .lines()
-   .filter_map(|line| line.ok())
-   .filter(|line| regex_set.is_match(line.as_str()))
-   .for_each(|x| println!("{}", x));
 
    Ok(())
 }
