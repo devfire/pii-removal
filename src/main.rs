6cefca9b03e54f5bddebcedbbf83b13f43df9ceb
Igor Kantor <igor@devfire.io>
2022-11-10 23:51:16 -0500

    Additional files (#5)
    * putting the files together
    
    * more comments
    
    * added bad data and more comments
    
    * added to .gitignore
    
    * working sample with redacted file
    
    * more comments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use flate2::read::GzDecoder;
 use regex::Regex;
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufWriter, Write, BufRead, BufReader};
 
 // NOTE the arg_required_else_help parameter. It forces a default help when no CLI inputs are passed.
 // It is undocumented but does exist, see here
@@ -22,34 +22,58 @@ fn main() -> io::Result<()> {
     // TODO: Should be configurable from the CLI.
     let pattern = "CC|SSN"; // make sure to return a &str here
 
+    let re = match Regex::new(pattern) {
+        Ok(re) => re,
+        Err(err) => panic!("{}", err),
+    };
+
+    // this gets appended to the end of the redacted file
+    let redacted_suffix = ".redacted";
+
     // cli.files is a Vector of strings, containing 1 or more files to process
     for file in cli.files {
-        println!("Processing file: {:?}", file);
+        print!("Processing file: {:?} ", file);
 
-        let file = File::open(file)?;
+        // constuct a new filename for the target output file with PII removed
+        // https://doc.rust-lang.org/std/macro.format.html
+        let redacted_file_name = format!("{}{}",file, redacted_suffix);
 
-        let reader = BufReader::new(GzDecoder::new(file));
+        // Open the gz input file read-only
+        let input_file = File::open(file)?;
+
+        // Create the file without PII write-only
+        let mut redacted_file = BufWriter::new(File::create(redacted_file_name)?);
+
+        // NOTE: the GzDecoder is already a buffered implementation. 
+        // However, it has no idea about any line breaks.
+        // Second BufReader is to identify the line breaks and return whole lines.
+        let reader = BufReader::new(GzDecoder::new(input_file));
      
+        // Total number of lines processed, per file
         let mut lines_processed = 0;
+
+        // Total number of PII lines removed, per file
         let mut lines_redacted = 0;
-     
-        let re = match Regex::new(pattern) {
-             Ok(re) => re,
-             Err(err) => panic!("{}", err),
-         };
-        
+
+        // Stream the gzip file contents, one line at a time
         for read_line_result in reader.lines() {
-            //println!("{}", line?);
+            
+            // Bump up the counter
             lines_processed = lines_processed + 1;
      
+            // read_line_result is a Result enum, so we will match on both
+            // OK() and Err() arms.
             match read_line_result {
                  Ok(read_line) => {
                      if re.is_match(&read_line) {
-                         println!("{}", read_line);
+                        //  println!("{}", read_line);
                          lines_redacted = lines_redacted + 1;
+                     } else {
+                        // println!("{}", read_line);
+                        writeln!(redacted_file, "{}", read_line)?;
                      }
                  },
-                 Err(e) => return Err(e),
+                 Err(e) => println!("Encountered invalid gzip file, error: {}", e)
             };
         }
         println!("Lines processed: {} Lines redacted: {}", lines_processed, lines_redacted);
